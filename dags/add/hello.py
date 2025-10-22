7762b82a3a159aa4a9e7e80f9e66886c1e1d7125
Igson Mendes <igson.mendes@gmail.com>
2025-10-21 20:53:29 -0400

    Update hello.py

--- dags/add/hello.py
+++ dags/add/hello.py
@@ -2,15 +2,13 @@ from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
 
-# Função que será executada pelo PythonOperator
 def minha_mensagem():
     print("Olá! Esta é a minha DAG básica funcionando no Airflow.")
 
-# Definição da DAG
 with DAG(
     dag_id="exemplo_print_mensagem",
     start_date=datetime(2025, 9, 9),
-    schedule_interval=None,  # Executa apenas quando manualmente disparada
+    schedule=None,   # <-- Trocar aqui!
     catchup=False
 ) as dag:
 
@@ -18,5 +16,3 @@ with DAG(
         task_id="print_mensagem",
         python_callable=minha_mensagem
     )
-
-    tarefa_print
